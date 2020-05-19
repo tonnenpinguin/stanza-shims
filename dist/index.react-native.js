f51d595e1d76b4b673cf6e4bc3d4515798ecfdff
Berni <bernhard@fittrack.io>
2020-05-19 18:30:51 +0200

    Use expo-random

--- dist/index.react-native.js
+++ dist/index.react-native.js
@@ -2,9 +2,19 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 exports.randomBytes = void 0;
 const tslib_1 = require('tslib');
-const RNRandomBytes = tslib_1.__importStar(require('react-native-randombytes'));
+const Random = tslib_1.__importStar(require('expo-random'));
 tslib_1.__exportStar(require('./index.browser'), exports);
+const RANDOM_NUMBER_SIZE = 128;
+let randomNumber;
+const getAndUpdate = (size) => {
+    const startIdx = Math.round((RANDOM_NUMBER_SIZE - size - 1) * Math.random());
+    const endIdx = startIdx + size;
+    const number = randomNumber?.subarray(startIdx, endIdx);
+    Random.getRandomBytesAsync(RANDOM_NUMBER_SIZE).then((val) => (randomNumber = val));
+    return number;
+};
+getAndUpdate(1);
 function randomBytes(size) {
-    return RNRandomBytes.randomBytes(size);
+    return Buffer.from(getAndUpdate(size));
 }
 exports.randomBytes = randomBytes;
--- src/index.react-native.ts
+++ src/index.react-native.ts
@@ -1,7 +1,21 @@
-import * as RNRandomBytes from 'react-native-randombytes';
+import * as Random from 'expo-random';
 
 export * from './index.browser';
 
+const RANDOM_NUMBER_SIZE = 128;
+let randomNumber: Uint8Array | undefined;
+
+const getAndUpdate = (size: number) => {
+    const startIdx = Math.round((RANDOM_NUMBER_SIZE - size - 1) * Math.random());
+    const endIdx = startIdx + size;
+    const number = randomNumber?.subarray(startIdx, endIdx);
+    Random.getRandomBytesAsync(RANDOM_NUMBER_SIZE).then((val) => (randomNumber = val));
+
+    return number;
+};
+
+getAndUpdate(1);
+
 export function randomBytes(size: number) {
-    return RNRandomBytes.randomBytes(size);
+    return Buffer.from(getAndUpdate(size)!);
 }
